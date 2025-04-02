2229d03cf7b3b3e90cf51cf6b75e2ae13b37a6ec
stev88712 <stev88712@gmail.com>
2025-04-03 08:49:14 +0900

    Add development log

---
+++ src/safe.sensenet_log.py
@@ -0,0 +1,15 @@
+# Development Log
+# Started at 2025-04-03 08:49:13
+
+# Fixed some bugs
+# Added documentation
+
+import json
+
+def save_to_json(data, filename):
+    with open(filename, 'w') as f:
+        json.dump(data, f, indent=4)
+
+def load_from_json(filename):
+    with open(filename, 'r') as f:
+        return json.load(f)
