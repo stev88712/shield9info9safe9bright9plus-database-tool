48a29fde5364405153618cef4ed4f59756abb477
stev88712 <stev88712@gmail.com>
2025-04-04 04:34:05 +0900

    Update development log at 2025-04-04 04:34:04

--- src/safe.sensenet_log.py
+++ src/safe.sensenet_log.py
@@ -13,3 +13,27 @@ def save_to_json(data, filename):
 def load_from_json(filename):
     with open(filename, 'r') as f:
         return json.load(f)
+
+# Update at 2025-04-04 04:34:04
+# Fixed some bugs
+# Added unit tests
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
