85403ad87a16bea075fd9b4297a7981aea5a3ef5
stev88712 <stev88712@gmail.com>
2025-04-17 14:58:11 +0900

    Update development log at 2025-04-17 14:58:11

--- src/safe.sensenet_log.py
+++ src/safe.sensenet_log.py
@@ -276,3 +276,15 @@ def is_prime(n):
         if n % i == 0:
             return False
     return True
+
+# Update at 2025-04-17 14:58:11
+# Improved performance
+
+
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
