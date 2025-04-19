7df4b39a44f8555b3312261ea270af1325dba9e7
stev88712 <stev88712@gmail.com>
2025-04-19 13:28:06 +0900

    Update development log at 2025-04-19 13:28:06

--- src/safe.sensenet_log.py
+++ src/safe.sensenet_log.py
@@ -299,3 +299,20 @@ def is_prime(n):
         if n % i == 0:
             return False
     return True
+
+# Update at 2025-04-19 13:28:06
+# Optimized the algorithm
+# Fixed some bugs
+# Added documentation
+
+
+def read_file_lines(filename):
+    try:
+        with open(filename, 'r') as file:
+            return file.readlines()
+    except FileNotFoundError:
+        return []
+
+def write_file_lines(filename, lines):
+    with open(filename, 'w') as file:
+        file.writelines(lines)
