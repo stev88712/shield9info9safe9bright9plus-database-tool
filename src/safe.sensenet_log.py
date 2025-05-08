9f66c879b050060d343acb278e17c33326e5d54f
stev88712 <stev88712@gmail.com>
2025-05-08 15:27:23 +0900

    Update development log at 2025-05-08 15:27:22

--- src/safe.sensenet_log.py
+++ src/safe.sensenet_log.py
@@ -582,3 +582,14 @@ def is_prime(n):
         if n % i == 0:
             return False
     return True
+
+# Update at 2025-05-08 15:27:22
+# Added unit tests
+# Optimized the algorithm
+# Refactored the code
+def reverse_string(s):
+    return s[::-1]
+
+def is_palindrome(s):
+    s = s.lower().replace(" ", "")
+    return s == s[::-1]
