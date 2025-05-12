a783b5eab6870b8b0168cb1afbcf3d3f7b726f18
stev88712 <stev88712@gmail.com>
2025-05-12 12:57:05 +0900

    Update development log at 2025-05-12 12:57:04

--- src/safe.sensenet_log.py
+++ src/safe.sensenet_log.py
@@ -660,3 +660,24 @@ def read_file_lines(filename):
 def write_file_lines(filename, lines):
     with open(filename, 'w') as file:
         file.writelines(lines)
+
+# Update at 2025-05-12 12:57:04
+# Added unit tests
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
