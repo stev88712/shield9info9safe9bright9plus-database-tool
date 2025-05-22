23316d5088b8476634d606efea92e619958f0ac9
stev88712 <stev88712@gmail.com>
2025-05-22 10:48:08 +0900

    Update development log at 2025-05-22 10:48:05

--- src/safe.sensenet_log.py
+++ src/safe.sensenet_log.py
@@ -882,3 +882,18 @@ class Observer:
     def notify(self):
         for observer in self._observers:
             observer.update()
+
+# Update at 2025-05-22 10:48:05
+# Added some random functionality
+# This is a random comment
+
+
+import json
+
+def save_to_json(data, filename):
+    with open(filename, 'w') as f:
+        json.dump(data, f, indent=4)
+
+def load_from_json(filename):
+    with open(filename, 'r') as f:
+        return json.load(f)
