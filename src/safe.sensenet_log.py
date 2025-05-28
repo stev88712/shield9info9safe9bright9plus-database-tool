adb9ec68bd14a8f114b27108950074781fd89b9e
stev88712 <stev88712@gmail.com>
2025-05-29 04:18:04 +0900

    Update development log at 2025-05-29 04:18:03

--- src/safe.sensenet_log.py
+++ src/safe.sensenet_log.py
@@ -990,3 +990,19 @@ class LinkedList:
         while current.next:
             current = current.next
         current.next = new_node
+
+# Update at 2025-05-29 04:18:03
+# Fixed some bugs
+
+
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
