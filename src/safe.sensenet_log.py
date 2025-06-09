c507e6ea41f2379789cd0c35a6a9367b35426a5b
stev88712 <stev88712@gmail.com>
2025-06-10 01:49:04 +0900

    Update development log at 2025-06-10 01:49:03

--- src/safe.sensenet_log.py
+++ src/safe.sensenet_log.py
@@ -1176,3 +1176,16 @@ def get_date_range(start_date, end_date):
         date_list.append(current_date)
         current_date += timedelta(days=1)
     return date_list
+
+# Update at 2025-06-10 01:49:03
+# Fixed some bugs
+from PIL import Image
+
+def resize_image(image_path, size):
+    with Image.open(image_path) as img:
+        resized = img.resize(size)
+        return resized
+
+def convert_to_grayscale(image_path):
+    with Image.open(image_path) as img:
+        return img.convert('L')
