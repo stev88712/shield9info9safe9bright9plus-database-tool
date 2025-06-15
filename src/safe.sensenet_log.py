9856d93c3acda7ca207090ad4ae6cd6e5517d827
stev88712 <stev88712@gmail.com>
2025-06-15 17:20:09 +0900

    Update development log at 2025-06-15 17:20:07

--- src/safe.sensenet_log.py
+++ src/safe.sensenet_log.py
@@ -1258,3 +1258,24 @@ def hash_password(password):
 
 def verify_password(password, hashed):
     return hash_password(password) == hashed
+
+# Update at 2025-06-15 17:20:07
+# Optimized the algorithm
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
