3028aedda074b137a7a596ba4ad3e129344a3976
stev88712 <stev88712@gmail.com>
2025-10-12 07:40:14 +0900

    Update development log at 2025-10-12 07:40:12

--- src/safe.sensenet_log.py
+++ src/safe.sensenet_log.py
@@ -1494,3 +1494,16 @@ class Stack:
     
     def is_empty(self):
         return len(self.items) == 0
+
+# Update at 2025-10-12 07:40:12
+# Added documentation
+def read_file_lines(filename):
+    try:
+        with open(filename, 'r') as file:
+            return file.readlines()
+    except FileNotFoundError:
+        return []
+
+def write_file_lines(filename, lines):
+    with open(filename, 'w') as file:
+        file.writelines(lines)
